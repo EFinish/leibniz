ff2519106ccaf0e901007f54c20b00afb2d2b6c0
EFinish <EFinish@protonmail.com>
2023-02-23 11:23:27 +0100

    fix docker compose to include fe, add mongo seed, wip for subjectmodal, actionbar, move TS protofiles to be within FE

---
+++ fe/src/lib/components/ActionBar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Button, Navbar, NavLi, NavUl } from 'flowbite-svelte';
+
+	const actionItems = [
+		{text: 'Create Subject'},
+		{text: 'Create Predicate'},
+		{text: 'Create Premise'},
+		{text: 'Create Proposition'},
+		{text: 'Create Conditional Statement'},
+		{text: 'Create Argument'}
+	];
+</script>
+
+<Navbar>
+	<NavUl>
+		{#each actionItems as actionItem}
+			<Button>{actionItem.text}</Button>
+		{/each}
+	</NavUl>
+</Navbar>
